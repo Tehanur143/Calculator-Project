197f520ddf9498c7448a6fe7b63dedfbd8d5c65d
Tehanur Abdulalim <teha_abdu1212@yahoo.com>
2023-04-04 21:07:46 -0400

    Added functionality to display user clicked button on screen of our calculator

--- text.js
+++ text.js
@@ -7,4 +7,16 @@
   let button = document.querySelectorAll(".btn");
   let clear = document.querySelector(".btn-clear");
   let equal = document.querySelector(".btn-equal");
+  // console.log(screen);
+  console.log(button);
+  // console.log(clear);
+  // console.log(equal);
+
+  // First Functionality
+  button.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      let userInput = e.target.dataset.num;
+      screen.value += userInput;
+    });
+  });
 })();
