b33769758da1e8a1bce91324c9608390ec814dba
Tehanur Abdulalim <teha_abdu1212@yahoo.com>
2023-04-06 17:12:37 -0400

    Add clear screen functionality to our clear button

--- text.js
+++ text.js
@@ -13,21 +13,30 @@
   // console.log(equal);
 
   // First Functionality
-  button.forEach(function (button) {
+  buttons.forEach(function (button) {
     button.addEventListener("click", function (e) {
       let userInput = e.target.dataset.num;
       screen.value += userInput;
-    });
-  });
+    })
+  })
 
-  // Second Functionality
+  // Second Functionality:Equal Button
   equal.addEventListener("click", function (e) {
     if (screen.value === "") {
       screen.value = "";
       console.log("equal button clicked");
     } else {
-      let answer = eval("screen.value");
+      // let calculatedResult = eval("2+5+3");
+      // console.log(calculatedResult);
+      let answer = eval(screen.value);
       screen.value = answer;
     }
+  })
+
+  // Third Functionality : clear Screen
+
+  clearInterval.addEventListener("click", function (e) {
+    screen.value = "";
+    // console.log("Clear button clicked")
   });
 })();
