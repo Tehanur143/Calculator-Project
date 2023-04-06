a1f83c95a0fb3d1a8ad20311906f83dd0db461b4
Tehanur Abdulalim <teha_abdu1212@yahoo.com>
2023-04-06 19:00:59 -0400

    Add clear screen functionality to our clear button

--- text.js
+++ text.js
@@ -1,26 +1,23 @@
 // IIFE
 
 (function () {
-  // Targetting the screen has-class-screen
+  // Targetting the screen has class screen
 
   let screen = document.querySelector(".screen");
-  let button = document.querySelectorAll(".btn");
+  let buttons = document.querySelectorAll(".btn");
   let clear = document.querySelector(".btn-clear");
   let equal = document.querySelector(".btn-equal");
-  // console.log(screen);
-  console.log(button);
-  // console.log(clear);
-  // console.log(equal);
+  console.log(buttons);
 
   // First Functionality
   buttons.forEach(function (button) {
     button.addEventListener("click", function (e) {
       let userInput = e.target.dataset.num;
       screen.value += userInput;
-    })
-  })
-
+    });
+  });
   // Second Functionality:Equal Button
+
   equal.addEventListener("click", function (e) {
     if (screen.value === "") {
       screen.value = "";
@@ -31,12 +28,12 @@
       let answer = eval(screen.value);
       screen.value = answer;
     }
-  })
+  });
 
   // Third Functionality : clear Screen
 
-  clearInterval.addEventListener("click", function (e) {
+  clear.addEventListener("click", function (e) {
     screen.value = "";
-    // console.log("Clear button clicked")
-  })
-})()
+    console.log("Clear button clicked");
+  });
+})();
